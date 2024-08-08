78fb795ffcfa8bc66479171fa4986b6e0069eb37
Joel Martelleur <joel.martelleur@gmail.com>
2024-08-08 20:25:57 +0200

    Updating about page style.

---
+++ src/assets/images/index.ts
@@ -0,0 +1,28 @@
+import calm1 from "./calm1.jpg";
+import calm2 from "./calm2.jpg";
+import calm3 from "./calm3.jpg";
+import calm4 from "./calm4.jpg";
+import calm5 from "./calm5.jpg";
+
+export const images = {
+    calm1: {
+        src: calm1,
+        alt: "Calm"
+    },
+    calm2: {
+        src: calm2,
+        alt: "Calm"
+    },
+    calm3: {
+        src: calm3,
+        alt: "Calm"
+    },
+    calm4: {
+        src: calm4,
+        alt: "Calm"
+    },
+    calm5: {
+        src: calm5,
+        alt: "Calm"
+    }
+};
--- src/features/Todo/components/About/About.tsx
+++ src/features/Todo/components/About/About.tsx
@@ -1,22 +1,47 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useRef } from "react";
 
 import { useTodoContext } from "../../context";
+import { images } from "../../../../assets";
 
-import styles from "./About.module.css"; 
+import styles from "./About.module.css";
 
 export const About = (): ReactElement => {
-    const [_, todoState] = useTodoContext();
-    
+    const [_, todoState] = useTodoContext();    
+    const imageRefs = useRef<HTMLDivElement[]>([]);
+    const imageSources = [
+        images.calm1.src,
+        images.calm2.src,
+        images.calm3.src,
+        images.calm4.src,
+        images.calm5.src
+    ];
+
     const getNrOfTodosNotDone = () => {
         return todoState.todoList.filter(item => item.todo.done === true).length;
-    } 
-    
+    }
+
+    useEffect(() => {
+        imageRefs.current.forEach((ref, index) => {
+            if (ref == null) { return; }
+            ref.style.backgroundImage = `url(${imageSources[index]})`;
+        });
+    }, [imageSources]);
+
     return (
-        <section>
-            <ul className={styles.aboutStatusList}>
-                <li>Total nr of todos: {todoState.todoList.length}</li>
-                <li>Nr of todos done: {getNrOfTodosNotDone()}</li>
-            </ul>
-        </section>
+        <>
+            <div>
+                <p>Total nr of todos: {todoState.todoList.length}</p>
+                <p>Nr of todos done: {getNrOfTodosNotDone()}</p>
+            </div>
+            <section className={styles.aboutSectionImageCtr}>
+                {imageSources.map((_, index) => (
+                    <div
+                        key={index}
+                        ref={(el) => (imageRefs.current[index] = el!)}
+                        className={styles[`box${index + 1}`]}
+                    ></div>
+                ))}
+            </section>
+        </>
     );
 }
